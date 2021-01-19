575d3bbf8acafffda511343571bce798721c9379
Luiz Franca <luizsoft@pop-os.localdomain>
2021-01-18 21:58:20 -0300

    updated

--- src/components/tableSections/Analise.tsx
+++ src/components/tableSections/Analise.tsx
@@ -8,12 +8,6 @@ import Line from "../Line";
 interface AnaliseProps extends React.InputHTMLAttributes<HTMLInputElement>{ }  
    
 
-
-   
-   
-
-
-
     const Analise = React.forwardRef<HTMLInputElement, AnaliseProps>((props) => {
        
 
@@ -23,22 +17,22 @@ interface AnaliseProps extends React.InputHTMLAttributes<HTMLInputElement>{ }
        
        
        
-
+function logCheckbox(){
+  console.log(checkBox);
+}
     return(
         
    <div>
-   
+  <button onClick={logCheckbox}/> 
     <div className="line">
     <Line name="Análise"    cols={[checkBox[0][0],checkBox[0][1],checkBox[0][2],checkBox[0][3],checkBox[0][4],checkBox[0][5]]}/>
     </div>
        
     <div className="line">
-   <Line  name="Análise de Contas" cols={[checkBox[1][0],checkBox[1][1],checkBox[1][2],checkBox[1][3],checkBox[1][4],checkBox[1][5]]}/>
 
     </div>
    
     <div className="line">
-    <Line name="Análise de Transações" cols={[checkBox[2][0],checkBox[2][1],checkBox[2][2],checkBox[2][3],checkBox[2][4],checkBox[2][5]]}/>
   
     </div>
      
